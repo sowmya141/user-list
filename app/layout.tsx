5a618f68950fdeab76d88f01ff39987a8832cfb7
Sowmya141 <sowmyaiyengar97@gmail.com>
2026-03-25 15:16:01 +0530

    Layout issue fixed

---
+++ app/layout.tsx
@@ -0,0 +1,16 @@
+export const metadata = {
+  title: "User App",
+  description: "User list application",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
